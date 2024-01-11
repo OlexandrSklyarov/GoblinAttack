edf8e783b303e75e9766603ff6c24b88be029d46
Sklyarov Olexandr <alexskrat@gmail.com>
2024-01-12 00:39:09 +0200

    [Update] Change hp bar.

---
+++ Assets/Scripts/Runtime/Core/UI/Hud/BaseHud.cs
@@ -0,0 +1,25 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+namespace Game.Runtime.Core.UI
+{
+    public abstract class BaseHud : MonoBehaviour
+    {    
+        [SerializeField] protected LineBarView _hpBar;
+        [SerializeField] protected TextMeshProUGUI _waveText; 
+
+        protected void OnChangeWaveProgress(int curWave, int wavesCount)
+        {
+            _waveText.text = $"WAVES: {curWave}/{wavesCount}";
+            _waveText.transform.DOPunchScale(Random.insideUnitCircle * 0.9f, 0.1f);
+        }
+
+        protected void OnChangeHealth(float progress)
+        {
+            _hpBar.SetProgress(progress);
+        }
+
+        public void Hide() => gameObject.SetActive(false);   
+    }
+}
--- Assets/Scripts/Runtime/Core/UI/LineBarView.cs
+++ Assets/Scripts/Runtime/Core/UI/LineBarView.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 namespace Game.Runtime.Core.UI
 {
     public class LineBarView : MonoBehaviour
     {        
-        [SerializeField] private Image _fill;
+        [SerializeField] private Image _fastBar;
+        [SerializeField] private Image _slowBar; 
+        [SerializeField, Min(0.01f)] private float _changeValueAnimationDuration = 0.5f;
+
+        private Tween _barTween;   
 
         public void SetProgress(float normValue)
         {
-            var value = Mathf.Clamp01(normValue);
-            _fill.fillAmount = value;
+            var value = Mathf.Clamp01(normValue);     
+
+            _barTween.Complete();
+
+            _barTween = _fastBar.DOFillAmount(value, _changeValueAnimationDuration)
+                .SetLink(_fastBar.gameObject)
+                .OnComplete(() => 
+                    _slowBar.DOFillAmount(value, _changeValueAnimationDuration)
+                    .SetLink(_slowBar.gameObject));
+        }        
+
+        private void OnDisable() 
+        {
+            _barTween?.Kill();
         }
     }
 }
